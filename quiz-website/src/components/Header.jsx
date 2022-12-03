9aff359fb74462ba5965f76382eef7b28780671d
moonbl3da <dominikkikina68@gmail.com>
2022-12-03 21:54:01 +0100

    Dinamikus header kész

--- quiz-website/src/components/Header.jsx
+++ quiz-website/src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom'; 
 
 export default function Header() {
+    const [pageState, setPageState] = useState("Sign in")
     const location = useLocation();
     const navigate = useNavigate();
+
+    const auth = getAuth();
+    useEffect(() => {
+        onAuthStateChanged(auth, (user)=>{
+            if(user)
+            {
+                setPageState("Profile");
+            }else
+            {
+                setPageState("Sign In");
+            }
+        })
+    }, [auth])
 
     function pathMatchRoute(route)
     {
@@ -12,6 +29,7 @@ export default function Header() {
             return true;
         }
     }
+
   return (
     <div className="bg-white border-b shadow-sm sticky top-0">
         <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -27,8 +45,9 @@ export default function Header() {
                     ${pathMatchRoute("/quiz-themes") && "text-black border-b-[#8c5000]"}`} 
                     onClick={() => navigate("/quiz-themes")}>Quiz themes</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-600 border-b-[3px] border-b-transparent 
-                    ${pathMatchRoute("/sign-in") && "text-black border-b-[#8c5000]"}`} 
-                    onClick={() => navigate("/sign-in")}>Sign In</li>
+                    ${ (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && 
+                    "text-black border-b-[#8c5000]"}`} 
+                    onClick={() => navigate("/profile")}>{pageState}</li>
                 </ul>
             </div>
         </header>
