2ae814a70f98efdf93e7af6305345d7d719c2c82
moonbl3da <dominikkikina68@gmail.com>
2022-12-03 18:42:09 +0100

    Profile oldalon adatok változtathatóak

--- quiz-website/src/pages/Profile.jsx
+++ quiz-website/src/pages/Profile.jsx
@@ -21,6 +21,18 @@ export default function Profile()
         auth.signOut()
         navigate("/");
     }
+
+    function onChange(e)
+    {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }));
+    }
+
+    function onSubmit(){
+        
+    }
     return (
         <>
             <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -28,16 +40,23 @@ export default function Profile()
                 <div className='w-full md:w-[50%] mt-6 px-3'>
                     <form>
                         {/*Name Input*/}
-                        <input type="text" id="name" value={name} disabled className='w-full px-4 py-2 mb-6 text-xl text-gray-600 
+                        <input type="text" id="name" value={name} disabled={!changeDetail} 
+                        onChange={onChange}
+                        className='w-full px-4 py-2 mb-6 text-xl text-gray-600 
                         bg-white border border-gray-300 rounded transition ease-in-out'/>
                          {/*Email Input*/}
-                         <input type="email" id="email" value={email} disabled className='w-full px-4 py-2 mb-6 text-xl text-gray-600 
+                         <input type="email" id="email" value={email} disabled={!changeDetail} 
+                         onChange={onChange}
+                         className='w-full px-4 py-2 mb-6 text-xl text-gray-600 
                         bg-white border border-gray-300 rounded transition ease-in-out'/>   
 
                         <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'> 
                             <p className='flex  items-center text-[#4F311C] '>Do you want to change your name?
                                 <span 
-                                    onClick={() => setChangeDetail((prevState) => !prevState )} 
+                                    onClick={() => {
+                                            changeDetail && onsubmit();
+                                            setChangeDetail((prevState) => !prevState);
+                                            }} 
                                         className='text-red-500 hover:text-red-800 transition ease-in-out duration-200 ml-2 
                                         cursor-pointer'>
                                     {changeDetail ? "Apply changes" : "Edit"}
