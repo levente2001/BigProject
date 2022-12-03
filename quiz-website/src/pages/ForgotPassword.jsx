9c3b29fa4c30e33629768fa4a08f034d80ffa85c
moonbl3da <dominikkikina68@gmail.com>
2022-12-03 16:21:05 +0100

    Elfelejtett jelszó oldal müködoképe. Kész

--- quiz-website/src/pages/ForgotPassword.jsx
+++ quiz-website/src/pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import OAuth from '../components/OAuth';
 
 export default function ForgotPassword() {
@@ -12,6 +14,19 @@ export default function ForgotPassword() {
    setEmail(e.target.value)
   }
 
+  async function onSubmit(e)
+  {
+    e.preventDefault();
+    try{
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth,email)
+      toast.success("Email was sent");
+    }catch(error)
+    {
+      toast.error("Could not send reset password!");
+    }
+  }
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
@@ -21,7 +36,7 @@ export default function ForgotPassword() {
           className="w-full rounded-2xl"/>
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="email" id="email" value={email} onChange={onChange} placeholder="Enter your email address..." className="mb-6 w-full px-4 py-2 text-xl text-gray-500 bg-white border-cyan-900 rounded transition ease-in-out" />
           <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
             <p className="text-white mb-6 ">Don't have an account? 
