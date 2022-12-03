9f184916f9ca5bb6c4ae6fb8ffb2b5c2d3df52e5
moonbl3da <dominikkikina68@gmail.com>
2022-12-03 21:31:12 +0100

    Profile oldal kész

--- quiz-website/src/pages/Profile.jsx
+++ quiz-website/src/pages/Profile.jsx
@@ -1,8 +1,10 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateCurrentUser, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
 import React from 'react';
 import { useState } from 'react';
 import { Navigate, useAsyncError, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 
 export default function Profile()
@@ -31,12 +33,23 @@ export default function Profile()
         }));
     }
 
-    function onSubmit(){
+    async function onSubmit(){
         try {
             if(auth.currentUser.displayName !== name)
             {
+                await updateProfile(auth.currentUser, {
+                    displayName: name,
+
+                })
+                
+                const docRef = doc(db, "users", auth.currentUser.uid);
+                await updateDoc(docRef , {
+                    name,
+
+                })
                 
             }
+            toast.success('Profile updated');
         } catch (error) {
             toast.error("Could not update the profile details!");
         }
@@ -64,7 +77,7 @@ export default function Profile()
                             <p className='flex  items-center text-[#4F311C] '>Do you want to change your name?
                                 <span 
                                     onClick={() => {
-                                            changeDetail && onsubmit();
+                                            changeDetail && onSubmit();
                                             setChangeDetail((prevState) => !prevState);
                                             }} 
                                         className='text-red-500 hover:text-red-800 transition ease-in-out duration-200 ml-2 
