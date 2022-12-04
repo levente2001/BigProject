d88c6683d7a6cb19e6ac032229d5ffedf2d11a90
Botond00 <botond.brudnyak@gmail.com>
2022-12-04 20:39:06 +0100

    Kémia kérdések feltöltve!

--- quiz-website/src/components/questions_data/kemia.js
+++ quiz-website/src/components/questions_data/kemia.js
@@ -1,73 +1,102 @@
 export default [
     {
-        question: '',
+        question: 'A vegyület ...',
         answers: [
-            '',
-            '',
-            '',
-            ''
-        ],
-        correct: 4
-    },
-
-    {
-        question: '',
-        answers: [
-            '',
-            '',
-            '',
-            ''
+            'olyan összetett anyag, amelyben az alkotórészek aránya állandó és meghatározott, kémiai változással egyszerűbb anyagokra bontható.',
+            'olyan egyszerűanyag, amelyben az alkotórészek aránya állandó és meghatározott, kémiai változással egyszerűbb anyagokra bontható.',
+            'olyan összetett anyag, amelyben az alkotórészek aránya tetszőleges, kémiai változással egyszerűbb anyagokra bontható.',
+            'olyan összetett anyag, amelyben az alkotórészek aránya állandó és meghatározott, fizikai változással egyszerűbb anyagokra bontható.'
         ],
         correct: 1
-    },    
+    },
     {
-        question: '',
+        question: 'Mit nevezünk desztillálásnak?',
         answers: [
-            '',
-            '', 
-            '', 
-            ''
-        ],    
-        correct: 4
-    },    
+            'Azt a műveletet, amely során egy folyadékelegy komponenseit gőzzé alakítjuk, majd a gőzt egy másik helyen lecsapatjuk.',
+            'Azt a műveletet, amely során egy gázelegy komponenseit folyadékká alakítjuk, majd a folyadékot egy másik helyen elpárologtatjuk.',
+            'Azt a műveletet, amely során egy szilárd anyag komponenseit gőzzé alakítjuk, majd a gőzt egy másik helyen lecsapatjuk.',
+            'Azt a műveletet, amely során egy folyadékelegy komponenseit szilárd anyaggá alakítjuk, majd a kristályt egy másik helyen elpárologtatjuk.'
+        ],
+        correct: 1
+    },
     {
-        question: '',
+        question: 'Mit nevezünk bepárlásnak?',
         answers: [
-            '',
-            '', 
-            '', 
-            ''
-        ],    
+            'Azt a folyamatot, amelynek során egy gázelegyből lecsapatjuk az oldószert, és visszamaradnak a nem illékony alkotórészek.',
+            'Azt a folyamatot, amelynek során egy oldatból kikristályosítjuk a nem illékony alkotórészeket, és visszamarad az oldószer.',
+            'Azt a folyamatot, amelynek során egy oldatból elpárologtatjuk az oldószert, és visszamaradnak a nem illékony alkotórészek.',
+            'Azt a folyamatot, amelynek során egy szilárd keverékből elpárologtatjuk az oldószert, és visszamaradnak az illékony alkotórészek.'
+        ],
         correct: 3
     },
     {
-        question: '',
+        question: 'Az elemek ...',
         answers: [
-            '',
-            '', 
-            '', 
-            ''
-        ],    
+            'olyan egyszerű anyagok, melyek fizikai változással egyszerűbb anyagokra bonthatók.',
+            'olyan összetett anyagok, melyek sem fizikai, sem kémiai változással nem bonthatók egyszerűbb anyagokra.',
+            'olyan egyszerű anyagok, melyek kémiai változással egyszerűbb anyagokra bonthatók.',
+            'olyan egyszerű anyagok, melyek sem fizikai, sem kémiai változással nem bonthatók egyszerűbb anyagokra.'
+        ],
+        correct: 4
+    },
+    {
+        question: 'A keverék ...',
+        answers: [
+            'olyan összetett anyag, melyben az alkotórészek aránya állandó, fizikai változással alkotórészeire bontható.',
+            'olyan összetett anyag, melyben az alkotórészek aránya tetszőleges, kémiai változással alkotórészeire bontható.',
+            'olyan egyszerű anyag, melyben az alkotórészek aránya tetszőleges, fizikai változással alkotórészeire bontható.',
+            'olyan összetett anyag, melyben az alkotórészek aránya tetszőleges, fizikai változással alkotórészeire bontható.'
+        ],
+        correct: 4
+    },
+    {
+        question: 'Ha telített az oldat ...',
+        answers: [
+            'az oldandó anyagból adott hőmérsékleten többet már nem lehet feloldani az oldószerben.',
+            'az oldott anyagból adott hőmérsékleten többet már nem lehet feloldani az oldószerben.',
+            'az oldandó anyagból többet már nem lehet feloldani az oldószerben.',
+            'az oldandó anyagból adott hőmérsékleten többet már nem lehet feloldani az oldatban.'
+        ],
         correct: 1
     },
     {
-        question: '',
+        question: 'Az oldat ...',
         answers: [
-            '',
-            '', 
-            '', 
-            ''
-        ],    
+            'Olyan egyszerű anyag, mely oldószerből és feloldott anyagból áll.',
+            'Olyan összetett anyag, mely oldószerből és oldandó anyagból áll.',
+            'Olyan összetett anyag, mely oldószerből és feloldott anyagból áll.',
+            'Olyan összetett anyag, mely oldatból és feloldott anyagból áll.'
+        ],
+        correct: 3
+    },
+    {
+        question: 'Az oldhatóság ...',
+        answers: [
+            'az anyagnak az a tulajdonsága, hogy adott mennyiségű oldószerben, adott hőmérsékleten mennyi oldódik fel belőle.',
+            'az anyagnak az a tulajdonsága, hogy adott mennyiségű oldatban, adott hőmérsékleten mennyi oldódik fel belőle.',
+            'az anyagnak az a tulajdonsága, hogy adott mennyiségű oldószerben, tetszőleges hőmérsékleten mennyi oldódik fel belőle.',
+            'az anyagnak az a tulajdonsága, hogy tetszőleges mennyiségű oldószerben, adott hőmérsékleten mennyi oldódik fel belőle.'
+        ],
         correct: 1
     },
     {
-        question: '',
+        question: 'A tömegszázalék megmutatja, hogy ...',
         answers: [
-            '',
-            '', 
-            '', 
-            ''
-        ],    
-        correct: 1
+            'az oldott anyag tömege hány százaléka az oldószer tömegének.',
+            'az oldott anyag tömege hány százaléka az oldat tömegének.',
+            'az oldat tömege hány százaléka az oldott anyag tömegének.',
+            'az oldószer tömege hány százaléka az oldat tömegének.'
+        ],
+        correct: 2
+    },
+    {
+        question: 'Az indikátor ...',
+        answers: [
+            'olyan kémiai tulajdonság, amely a kémhatást színváltozással jelzi.',
+            'olyan kémiai anyag, amely a kémhatást színváltozással jelzi.',
+            'olyan fizikai anyag, amely a kémhatást színváltozással jelzi.',
+            'olyan kémiai anyag, amely a kémhatást halmazállapot-változással jelzi.'
+        ],
+        correct: 2
     }
 ]
